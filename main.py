d23fd19276c96e9ae3b056e9c3dfab8f75865ab3
piexian <64474352+piexian@users.noreply.github.com>
2025-07-12 06:28:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from typing import Dict, List, Optional
     "revolver_game",
     "原作者：zgojin，修正者：piexian",
     "群聊左轮手枪游戏插件，支持随机走火事件",
-    "1.8.0",
+    "1.7.0",
     "https://github.com/piexian/astrbot_plugin_rg"
 )
 class RevolverGamePlugin(Star):
