b80800c01de6ea42d0b2258132552f4d79cad817
piexian <64474352+piexian@users.noreply.github.com>
2025-07-28 09:36:25 +0800

    增加游戏结束语

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ class RevolverGamePlugin(Star):
             yaml.dump(texts, file, allow_unicode=True)
 
     @event_message_type(EventMessageType.ALL)
-    async def on_all_messages(self, event: AstrMessageEvent):
+    async def on_all_messages(self, event: AstrMessageEvent, *args, **kwargs):
         """处理所有消息"""
         group_id = self._get_group_id(event)
         is_private = not group_id  # 判断是否为私聊
